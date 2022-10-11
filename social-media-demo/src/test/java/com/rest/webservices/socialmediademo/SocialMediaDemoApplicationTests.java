56b82eed79ed8e89e720eb23bab850b1da6dcc2a
kettyPek <ketty.pekarsky@gmail.com>
2022-10-11 16:24:13 +0300

    initial commit

---
+++ social-media-demo/src/test/java/com/rest/webservices/socialmediademo/SocialMediaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.webservices.socialmediademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
