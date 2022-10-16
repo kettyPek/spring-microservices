a316b6e52069ceda86a32507906c2e81aeba2bd4
kettyPek <ketty.pekarsky@gmail.com>
2022-10-16 10:54:59 +0300

    Add Post request

--- social-media-demo/src/main/java/com/rest/webservices/socialmediademo/dao/UserDaoService.java
+++ social-media-demo/src/main/java/com/rest/webservices/socialmediademo/dao/UserDaoService.java
@@ -11,12 +11,14 @@ import com.rest.webservices.socialmediademo.models.User;
 @Component
 public class UserDaoService {
 	
+	private static Integer usersCount = 0;
+	
 	private static List<User> users = new ArrayList<User>();
 	
 	static {
-		users.add(new User(1,"adam",LocalDate.of(1996,2,2)));
-		users.add(new User(2,"arik",LocalDate.of(1992,3,4)));
-		users.add(new User(3,"lina",LocalDate.of(1994,2,3)));
+		users.add(new User(usersCount++,"adam",LocalDate.of(1996,2,2)));
+		users.add(new User(usersCount++,"arik",LocalDate.of(1992,3,4)));
+		users.add(new User(usersCount++,"lina",LocalDate.of(1994,2,3)));
 	}
 	
 	public List<User> finalAll(){
@@ -25,6 +27,11 @@ public class UserDaoService {
 	
 	public User findById(int id) {
 		 return users.stream().filter(u -> u.getId() == id).findFirst().get();
+	}
+	
+	public void createUser(User user) {
+		user.setId(usersCount++);
+		users.add(user);
 	}
 
 }
